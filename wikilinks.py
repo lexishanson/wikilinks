1f8951253251c1613359e138e30a264e6b140199
lexishanson <lexishanson@gmail.com>
2016-11-28 22:13:11 -0700

    Given a date, check for a link to Wikipedia about that date, and open that link in the browser

---
+++ wikilinks.py
@@ -0,0 +1,19 @@
+import datetime
+import webbrowser
+
+answer_format = '%m/%d'
+link_format = '%b_%d'
+link = 'https://en.wikipedia.org/wiki/{}'
+
+while True:
+    answer = input("What date would you like? Please use the MM/DD format. Enter 'quit' to quit. ")
+    if answer.upper() == 'QUIT':
+        break
+        
+    try:
+        date = datetime.datetime.strptime(answer, answer_format)
+        output = link.format(date.strftime(link_format))
+        webbrowser.open(output, new=2, autoraise=True)
+        print(output)
+    except ValueError:
+        print("That's not a valid date. Please try again.")
